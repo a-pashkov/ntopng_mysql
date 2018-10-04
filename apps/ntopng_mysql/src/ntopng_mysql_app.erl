cba4d0e89bb9cce079543c063aa94de28bee2551
Administrator <admin@mipt-116-4-6.(none)>
2018-10-04 14:31:27 +0400

    start

---
+++ apps/ntopng_mysql/src/ntopng_mysql_app.erl
@@ -0,0 +1,16 @@
+-module(ntopng_mysql_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ntopng_mysql_sup:start_link().
+
+stop(_State) ->
+    ok.
