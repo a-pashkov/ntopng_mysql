00cc7a81f2d42ee0f9ac1dd6262734fc87baad00
Administrator <admin@mipt-116-4-6.(none)>
2018-10-27 11:54:28 +0300

    multiple tables

--- apps/ntopng_mysql/src/ntopng_mysql.erl
+++ apps/ntopng_mysql/src/ntopng_mysql.erl
@@ -6,7 +6,24 @@
 -export([start_link/0]).
 -export([init/1, handle_cast/2, handle_call/3, handle_info/2, terminate/2, code_change/3]).
 
+-record(packet, {
+  ntop_timestamp, 
+  src_addr, 
+  src_port, 
+  dst_addr, 
+  dst_port, 
+  in_bytes, 
+  out_bytes, 
+  in_pkts, 
+  out_pkts, 
+  first_switched, 
+  last_switched, 
+  ntopng_instance_name, 
+  interface}).
 -record(state, {data=[], mysqlpid, timer, socket}).
+
+-define(UNIX_EPOCH, 62167219200).
+-define(DAY, 86400).
 
 -define(BUFF_SIZE, 2000). 
 -define(UDP_PORT, 5510).
@@ -86,61 +103,124 @@ extract(Data)->
     {_, LastSwitched} = lists:keyfind(<<"LAST_SWITCHED">>, 1, Data1),
     {_, NtopngInstanceName} = lists:keyfind(<<"NTOPNG_INSTANCE_NAME">>, 1, Data1),
     {_, Interface} = lists:keyfind(<<"INTERFACE">>, 1, Data1),
-    {ok, [
-      binary_to_list(<<"'", NtopTimestamp/binary, "'">>), 
-      binary_to_list(<<"INET_ATON('", SrcAddr/binary, "')">>),
-      integer_to_list(SrcPort),
-      binary_to_list(<<"INET_ATON('", DstAddr/binary, "')">>),
-      integer_to_list(DstPort),
-      integer_to_list(InPkts),
-      integer_to_list(InBytes),
-      integer_to_list(OutPkts), 
-      integer_to_list(OutBytes),
-      integer_to_list(FirstSwitched), 
-      integer_to_list(LastSwitched), 
-      binary_to_list(<<"'", NtopngInstanceName/binary, "'">>), 
-      binary_to_list(<<"'", Interface/binary, "'">>)]} 
+    {true, #packet{ 
+      ntop_timestamp=NtopTimestamp, 
+      src_addr=SrcAddr,
+      src_port=SrcPort,
+      dst_addr=DstAddr,
+      dst_port=DstPort,
+      in_pkts=InPkts,
+      in_bytes=InBytes,
+      out_pkts=OutPkts, 
+      out_bytes=OutBytes,
+      first_switched=FirstSwitched, 
+      last_switched=LastSwitched, 
+      ntopng_instance_name=NtopngInstanceName, 
+      interface=Interface}} 
   catch
     error:Error->
       lager:info("~p ~p~nData:~n~p~n", [Error, erlang:get_stacktrace(), Data]),
       error
   end.
 
-mysql_format([], Acc)->
-    string:join(Acc, "),(");
-mysql_format([HData|TData], Acc)->
-  case extract(HData) of
-    {ok, Rec}->
-      mysql_format(TData, [string:join(Rec, ",")|Acc]);
-    _Else->
-      mysql_format(TData, Acc)
-  end.
+%mysql_format([], Acc)->
+%    string:join(Acc, "),(");
+%mysql_format([HData|TData], Acc)->
+%  case extract(HData) of
+%    {ok, Rec}->
+%      mysql_format(TData, [string:join(Rec, ",")|Acc]);
+%    _Else->
+%      mysql_format(TData, Acc)
+%  end.
 
-mysql_insert(MySqlPid, Data)-> 
-  %lager:info("~p~n", [Data]),
-  Query = "INSERT INTO flows (
-    NTOP_TIMESTAMP, 
-    IP_SRC_ADDR, 
-    L4_SRC_PORT, 
-    IP_DST_ADDR, 
-    L4_DST_PORT, 
-    IN_PACKETS,  
-    IN_BYTES, 
-    OUT_PACKETS, 
-    OUT_BYTES, 
-    FIRST_SWITCHED, 
-    LAST_SWITCHED, 
-    NTOPNG_INSTANCE_NAME, 
-    INTERFACE) 
-    VALUES (",
-    ok = mysql:query(MySqlPid, Query ++ Data ++ ")").
+%mysql_insert(MySqlPid, Data)-> 
+%  %lager:info("~p~n", [Data]),
+%  Query = "INSERT INTO flows (
+%    NTOP_TIMESTAMP, 
+%    IP_SRC_ADDR, 
+%    L4_SRC_PORT, 
+%    IP_DST_ADDR, 
+%    L4_DST_PORT, 
+%    IN_PACKETS,  
+%    IN_BYTES, 
+%    OUT_PACKETS, 
+%    OUT_BYTES, 
+%    FIRST_SWITCHED, 
+%    LAST_SWITCHED, 
+%    NTOPNG_INSTANCE_NAME, 
+%    INTERFACE) 
+%    VALUES (",
+%    ok = mysql:query(MySqlPid, Query ++ Data ++ ")").
+
+mysql_insert(BPeriod, Data, MySqlPid)-> 
+  DataGroups = lists:map(fun(Packet)-> 
+    #packet{
+      ntop_timestamp=NtopTimestamp,
+      src_addr=SrcAddr,
+      src_port=SrcPort,
+      dst_addr=DstAddr,
+      dst_port=DstPort,
+      in_pkts=InPkts,
+      in_bytes=InBytes,
+      out_pkts=OutPkts,
+      out_bytes=OutBytes,
+      first_switched=FirstSwitched,
+      last_switched=LastSwitched,
+      ntopng_instance_name=NtopngInstanceName,
+      interface=Interface} = Packet, 
+    string:join([
+      binary_to_list(<<"'", NtopTimestamp/binary, "'">>), 
+      binary_to_list(<<"INET_ATON('", SrcAddr/binary, "')">>), 
+      integer_to_list(SrcPort), 
+      binary_to_list(<<"INET_ATON('", DstAddr/binary, "')">>),
+      integer_to_list(DstPort),
+      integer_to_list(InPkts),
+      integer_to_list(InBytes),
+      integer_to_list(OutPkts),
+      integer_to_list(OutBytes),
+      integer_to_list(FirstSwitched),
+      integer_to_list(LastSwitched),
+      binary_to_list(<<"'", NtopngInstanceName/binary, "'">>), 
+      binary_to_list(<<"'", Interface/binary, "'">>)], ",") end, Data), 
+  DataPart = string:join(DataGroups, "),("), 
+  {{Year, Month, Day}, _Time} = timestamp_to_localtime(BPeriod), 
+  TableName = lists:flatten(io_lib:format("flows_~4..0w~2..0w~2..0w", [Year, Month, Day])), 
+  Query = "INSERT INTO " ++ TableName  ++ " (
+    NTOP_TIMESTAMP,
+    IP_SRC_ADDR,
+    L4_SRC_PORT,
+    IP_DST_ADDR,
+    L4_DST_PORT,
+    IN_PACKETS,
+    IN_BYTES,
+    OUT_PACKETS,
+    OUT_BYTES,
+    FIRST_SWITCHED,
+    LAST_SWITCHED,
+    NTOPNG_INSTANCE_NAME,
+    INTERFACE) VALUES (" ++ DataPart ++ ")", 
+    ok = mysql:query(MySqlPid, Query).
 
 send([],_MySqlPid)->
   lager:info("Empty data~n");
 send(Data, MySqlPid)->
   lager:info("Send data length: ~p~n", [length(Data)]),
   %lager:info("Data: ~n~p~n", [mysql_format(Data, [])]),
-  mysql_insert(MySqlPid, mysql_format(Data, [])).
+  %mysql_insert(MySqlPid, mysql_format(Data, [])).
+  Data1 = lists:filtermap(fun extract/1, Data), 
+  send1(lists:keysort(#packet.last_switched, Data1), MySqlPid).
+
+send1([], _)-> 
+  ok; 
+send1([HData|_]=Data, MySqlPid)->
+  BPeriod = localtime_start_day(HData#packet.last_switched), % Начало дня
+  {InPeriod, OutPeriod} = lists:splitwith(fun(#packet{last_switched=LastSwitched})-> 
+                                            (LastSwitched >= BPeriod) and (LastSwitched < BPeriod + ?DAY) end, Data), 
+  mysql_insert(BPeriod, InPeriod, MySqlPid), 
+  %lager:info("Period: ~p~nInPeriod:~n~p~n", [
+  %  timestamp_to_localtime(BPeriod), 
+  %  [timestamp_to_localtime(Data1#packet.last_switched) || Data1 <- InPeriod]]), 
+  send1(OutPeriod, MySqlPid).
 
 start_timer(undefined)-> 
   lager:info("Data transfer started~n",[]), 
@@ -152,4 +232,15 @@ stop_timer(undefined)->
   undefined;
 stop_timer(Time)-> 
   lager:info("Data transfer took ~p seconds~n", [timer:now_diff(erlang:now(), Time) div 1000000]).
+
+localtime_start_day(Timestamp) -> 
+  {Date, _Time} = timestamp_to_localtime(Timestamp), 
+  localtime_to_timestamp({Date, {0, 0, 0}}).
+
+timestamp_to_localtime(Timestamp) -> 
+  calendar:universal_time_to_local_time(calendar:gregorian_seconds_to_datetime(?UNIX_EPOCH + Timestamp)).
+
+localtime_to_timestamp(Localtime) -> 
+  [Utc] = calendar:local_time_to_universal_time_dst(Localtime),  
+  calendar:datetime_to_gregorian_seconds(Utc) - ?UNIX_EPOCH.
 
